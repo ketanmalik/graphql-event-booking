02ab92a4ea1c08ea8d640b24bf578a27243e7ec9
ketanmalik <ketanmalik@gmail.com>
2020-07-04 13:22:52 -0500

    first-basic-graphql-ap

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const grapgQlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  grapgQlHttp({
+    schema: buildSchema(`type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }`),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Coding", "React"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000);
