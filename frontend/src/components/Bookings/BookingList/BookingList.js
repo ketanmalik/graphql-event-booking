7716c5d93f650bfa9392ddd3a994ff617905c622
ketanmalik <ketanmalik@gmail.com>
2020-07-11 23:35:52 -0500

    cancel-booking

---
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./BookingList.css";
+
+const bookingList = (props) => {
+  return (
+    <ul className="bookings__list">
+      {props.bookings.map((booking) => {
+        return (
+          <li key={booking._id} className="bookings__item" key={booking._id}>
+            <div className="bookings__item-data">
+              {booking.event.title} -
+              {new Date(booking.createAt).toLocaleDateString()}
+            </div>
+            <div className="bookings__item-control">
+              <button
+                className="btn"
+                onClick={props.onCancel.bind(this, booking._id)}
+              >
+                Cancel
+              </button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default bookingList;
--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import AuthContext from "../context/auth-context";
 import Spinner from "../components/Spinner/Spinner";
+import BookingList from "../components/Bookings/BookingList/BookingList";
 
 class BookingsPage extends Component {
   state = {
@@ -56,19 +57,60 @@ class BookingsPage extends Component {
       });
   };
 
+  cancelBookingHandler = (bookingId) => {
+    this.setState({ isLoading: true });
+
+    const requestBody = {
+      query: `
+              mutation {
+                  cancelBooking(bookingId: "${bookingId}"){
+                      _id
+                      title
+                      description
+                  }
+              }
+              `,
+    };
+    const token = this.context.token;
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.data !== 201) {
+          console.log("Failed!");
+          return;
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState((prevState) => {
+          const updatedBookings = prevState.bookings.filter((booking) => {
+            return booking._id !== bookingId;
+          });
+          return { bookings: updatedBookings, isLoading: false };
+        });
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false });
+        throw err;
+      });
+  };
+
   render() {
     return (
       <React.Fragment>
         {this.state.isLoading ? (
           <Spinner />
         ) : (
-          <ul>
-            {this.state.bookings.map((booking) => (
-              <li key={booking._id}>
-                {booking.event.title} - {booking.createAt}
-              </li>
-            ))}
-          </ul>
+          <BookingList
+            bookings={this.state.bookings}
+            onCancel={this.cancelBookingHandler}
+          />
         )}
       </React.Fragment>
     );
